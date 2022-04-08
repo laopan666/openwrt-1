9627831ecb4b88078121438cbb2bfbd5d2409ecc
laopan666 <yospan@qq.com>
2022-04-08 17:06:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,6 +16,8 @@
 # Add a feed source
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/myapp/luci-app-adguardhome
 git clone https://github.com/2512500960/zzu-minieap-openwrt package/myapp/zzu-minieap-openwrt
 git clone https://github.com/tty228/luci-app-serverchan package/myapp/luci-app-serverchan
